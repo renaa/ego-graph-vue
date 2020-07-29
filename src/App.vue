193bda30faecdb29cdb22bfe9cc470100ffca97d
tomas <tomasrenaa@gmail.com>
2020-07-29 23:08:45 +0200

    styling

--- src/App.vue
+++ src/App.vue
@@ -1,77 +1,160 @@
 <template>
   <div id="app">
-
-    <Query @result="onDataFromServer"/>
+    <h1 class="egograph">
+      Egograph
+      <div class="tooltiptext">
+        An ego graph is a great way to get to know a new thing. It is graphs where we can see connected terms together and how they compare. This is done by recursively googling whatever you serch for, and adding vs (versus). fex. "puppies vs" and seeing the search results that come up, then doing the same to them. 
+      <a class="egograph" href="https://medium.com/applied-data-science/the-google-vs-trick-618c8fd5359f">here is an article about it</a>
+        <br> The thicker the line, the stronger the connection.
+        </div>
+      </h1>
+    <Query @result="onDataFromServer" />
+    <div class="d3Wrapper">
 
     <d3-network class="d3-network" :net-nodes="nodes" :net-links="links" :options="options" />
-
+    </div>
   </div>
 </template>
 
 <script>
 import Query from "./components/Query"
-import D3Network from 'vue-d3-network'
+import D3Network from "vue-d3-network"
 
 export default {
   name: "App",
   components: {
     Query,
     D3Network,
-
   },
   data: function() {
     return {
       response: "",
-      
+
       isOpen: false,
 
       nodes: [],
       links: [],
-      options:{
-      canvas: false,
+      options: {
+        canvas: false,
         force: 3000,
         nodeLabels: true,
-        nodeSize: 20,
+        // nodeSize: 20,
         fontSize: 20,
-
+        // linkWidth: 1,
       },
     }
   },
   methods: {
     onDataFromServer(value) {
-      console.log(value.Item1, value.Item1.length === 0)
-      if(value.Item1.length === 0){
-        console.log('no result from server')
-      }
-      else{
-      this.nodes = value.Item1.map(x => {
-        return {
-          ...x,
-          _color: 'gray'
-        }
-      })
-      this.links = value.Item2.map(x => {
-        return{
-          ...x, 
-          _color: 'gray'
-        }
-      });
-      }
-    },
-    
+      if (value.Item1.length === 0) {
+        console.log("no result from server")
+      } else {
+        this.nodes = value.Item1.map(x => {
+          
+          return {
+            ...x,
+            // _color: "gray",
+            _size: x === value.Item1[0] ? 35 : 20,
+             _labelClass: x === value.Item1[0] ? 'rootNodeLabel d3Label' : 'nodeLabel d3Label',
 
+          }
+        })
+        this.links = value.Item2.map(x => {
+          //x.name is strength of connection
+          let colorspectrum = '#073b4c'
+          let widths = [1, 3, 5, 7, 6]
+          let color = colorspectrum
+          let width = widths[0]
+
+          if (x.name > 20) {
+            color = colorspectrum
+            width = widths[4]
+          } else if (x.name > 15) {
+            color = colorspectrum
+            width = widths[3]
+          } else if (x.name > 10) {
+            color = colorspectrum
+            width = widths[2]
+          } else if (x.name > 5) {
+            color = colorspectrum
+            width = widths[1]
+          }
+
+          return {
+            ...x,
+            // _color: color,
+            _svgAttrs: {
+              stroke: color,
+              "stroke-width": width,
+            },
+          }
+        })
+      }
+      console.log(
+        value.Item2.map(x => {
+          return {
+            ...x,
+            // _color: "blue",
+          }
+        })
+      )
+    },
   },
 }
 </script>
 
 <style lang="scss">
+body{
+background: rgb(239,71,111);
+background: linear-gradient(351deg, rgba(239,71,111,1) 0%, rgba(255,209,102,1) 95%);
+margin: 0;
+}
+a {
+  text-decoration: none;
+  color: #8cffc9 !important;
+  font-size: 1rem !important;
+}
+.egograph{
+  position: relative;
+  display: inline-block;
+  color: white;
+  margin-top: 0;
+  font-size: 2.3rem;
+  text-shadow: 0px 10px 20px rgba(0, 0, 0, 0.7);
+  width: 180;
+}
+.egograph:hover{
+  color: #076460;
+
+}
+.tooltiptext{
+  border-radius: 5px;
+  padding: 15px;
+  margin-top: 12px ;
+  visibility: hidden;
+  font-size: 1rem;
+  position: absolute;
+  left: -97px;
+  width: 360px;
+  font-weight: 200;
+  
+  background-color: #073b4cbf;
+  margin: 0;
+  text-align: center;
+}
+.egograph:hover .tooltiptext{
+  visibility: visible;
+  color: white;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: #076460;
+  margin-top: 30px;
+  overflow: hidden;
   // display: flex;
   // flex-direction: column;
   // align-items: center;
@@ -86,7 +169,33 @@ export default {
   transition: opacity 0.5s ease-out;
 }
 
-.d3-network{
-  height: 100vh;
+.d3-network {
+  height: 91vh;
+  width: 100%;
+  
+}
+.node{
+  fill: #073b4c;
+  stroke: #8cffc9;
+  // stroke:#06d6a0;
+  stroke-width: 4px;
+}
+.d3Label{
+  fill: white;
+  cursor: unset;
+  user-select: none;
+
+
+}
+.nodeLabel{
+  font-size: 20px;
+  
+}
+.rootNodeLabel{
+  font-size: 30px;
+}
+
+.d3Wrapper{
+  z-index: 3;
 }
 </style>
--- src/components/Query.vue
+++ src/components/Query.vue
@@ -1,8 +1,10 @@
 <template>
-<div>
-  <input type="text" v-model="query" @keyup.enter="getData" />
-  <button @click="getData">search</button>
-</div>
+  <div class="cube">
+
+      <input type="text" v-model="query" @keyup.enter="getData" placeholder="search for something"/>
+      <button @click="getData">search</button>
+
+  </div>
 </template>
 
 <script>
@@ -11,28 +13,59 @@ const axios = require("axios")
 export default {
   data() {
     return {
-      query: ''
+      query: "",
+      searching: false,
     }
   },
   methods: {
     getData() {
-      this.$emit('searching')
+      this.searching = true,
+      this.$emit("searching")
       var url = "https://uniquefunctionname.azurewebsites.net/api/EgoGraph?name=" + this.query
-      // var url = "http://localhost:7071/api/EgoGraph?name=" + this.query
+      // var url = "http://localhost:7071/api/EgoGraph?name=" + this.query // local function
       axios
         .get(url)
         .then(response => {
-          if (response.status === 200){
-            this.$emit('result', response.data)
-          } 
-          else {
-            console.log('server problems:', {response})
+          if (response.status === 200) {
+            this.$emit("result", response.data)
+          } else {
+            console.log("server problems:", { response })
           }
         })
         .catch(error => console.log(error))
+      this.searching = false
     },
   },
 }
 </script>
 
-<style></style>
+<style scoped>
+.cube{
+    background-color: white;
+  height: 50px;
+  width: 300px;
+  box-shadow: 15px 15px 50px rgba(0,0,0,0.5);
+  border-radius: 5px;
+  margin: auto;
+  display: flex;
+  padding: 0 8px 0 15px;
+  justify-content: space-between;
+  align-items: center;
+}
+input {
+  border: none;
+  outline: none;
+  height: 90%;
+  font-size: 1.1em;
+}
+button{
+  border: none;
+  color: #8cffc9;
+  background-color: #073b4c;
+  height: 25px;
+  outline: none;
+  border-radius: 3px;
+  height: 70%;
+  
+}
+</style>
